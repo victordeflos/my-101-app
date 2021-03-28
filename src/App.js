34fd86086159f513446d24764ce1102ef94f6901
Viktor Blazhevski <victordeflos@hotmail.com>
2021-03-28 18:33:48 +0200

    react props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+/** @format */
+
 import './App.css';
+import Item from './MyItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Item name='ContactBook' />
+        <Item name='MovieInformant' />
+        <Item name='Info Posts' />
       </header>
     </div>
   );
---
+++ src/MyItem.js
@@ -0,0 +1,9 @@
+/** @format */
+import React from 'react';
+
+class Item extends React.Component {
+  render() {
+    return <h1>My App {this.props.name}</h1>;
+  }
+}
+export default Item;
